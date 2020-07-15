93b841119573b9b746c31db2d02f975ee631056a
camiloaconcha <camiloaconchaa@gmail.com>
2020-07-15 00:06:42 -0500

    Updated scripts

---
+++ back_npm.sh
@@ -0,0 +1,11 @@
+##
+
+echo "Installing Backend 🔨🔨🔨 ######"
+cd mercado_api
+
+echo "Installing Backend Dependencies"
+npm install
+
+echo " Running Backend 🚀🚀🚀 "
+yarn start
+
---
+++ front_node.sh
@@ -0,0 +1,9 @@
+echo 'Installing Front End 🔨🔨🔨'
+
+cd mercado_app
+
+echo 'Installing Front End Dependencies 🔨🔨🔨'
+npm install
+
+echo 'Running Front End 🚀🚀🚀 '
+npm start
